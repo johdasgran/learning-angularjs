1e785b21d3c973cdf7cf6131c84177a262046eae
Johan Grandas <dampig14@gmail.com>
2023-04-04 22:34:20 -0500

    ng-model

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ var app = angular.module("app", []);
 app.controller("emp", ["$scope", function($scope) {
   $scope.a = 10;
   $scope.b = 20;
+
+  $scope.doSum = function () {
+    $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+  }
+
+  // $scope.doDoubleAndSum = function () {
+  //   $scope.a = $scope.a * 2;
+  //   $scope.b = $scope.b * 2;
+  //   $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+  // }
+
+
 }])
