ba8b50e6616c22156f7aeb9f24b7a7c50c0f97ec
Johan Grandas <dampig14@gmail.com>
2023-04-06 12:40:05 -0500

    factory

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var app = angular.module("app", []);
 
-app.controller("emp", ["$scope", "$http", function($scope, $http) {
+app.controller("emp", ["$scope", "calcFactory", function($scope, calcFactory) {
   $scope.a = 10;
   $scope.b = 20;
 
   $scope.doSum = function () {
-    $scope.sum = parseInt($scope.a) + parseInt($scope.b);
-  }
-
-
-  $http({
-    url: 'https://finalspaceapi.com/api/v0/character',
-    method: 'GET'
-  }).then(function (resp) {
-    //success function
-    console.log(resp)
-
-  },
-  function (resp) {
-    //failure function
-    console.log(resp)
+    // $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+    $scope.sum = calcFactory.getSum($scope.a, $scope.b)
 
   }
-  )
 
+  // $http({
+  //   url: 'https://finalspaceapi.com/api/v0/character',
+  //   method: 'GET'
+  // }).then(function (resp) {
+  //   //success function
+  //   console.log(resp)
+
+  // },
+  // function (resp) {
+  //   //failure function
+  //   console.log(resp)
+
+  // }
+  // )
 
 
   // $scope.doDoubleAndSum = function () {
@@ -32,5 +32,18 @@ app.controller("emp", ["$scope", "$http", function($scope, $http) {
   //   $scope.sum = parseInt($scope.a) + parseInt($scope.b);
   // }
 
+
+}]);
+
+
+app.factory('calcFactory', ['$http', '$log', function($http, $log){
+  $log.log('instanting calcFactory..')
+
+  var oCalcService = {}
+  oCalcService.getSum = function(a, b) {
+    return parseInt(a) + parseInt(b);
+  }
+
+  return oCalcService;
 
 }])
