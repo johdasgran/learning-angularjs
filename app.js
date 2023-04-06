f3ab3281390ce0d795120a4f906805ad10d7f5ef
Johan Grandas <dampig14@gmail.com>
2023-04-05 22:27:31 -0500

    http

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var app = angular.module("app", []);
 
-app.controller("emp", ["$scope", function($scope) {
+app.controller("emp", ["$scope", "$http", function($scope, $http) {
   $scope.a = 10;
   $scope.b = 20;
 
   $scope.doSum = function () {
     $scope.sum = parseInt($scope.a) + parseInt($scope.b);
   }
+
+
+  $http({
+    url: 'https://finalspaceapi.com/api/v0/character',
+    method: 'GET'
+  }).then(function (resp) {
+    //success function
+    console.log(resp)
+
+  },
+  function (resp) {
+    //failure function
+    console.log(resp)
+
+  }
+  )
+
+
 
   // $scope.doDoubleAndSum = function () {
   //   $scope.a = $scope.a * 2;
