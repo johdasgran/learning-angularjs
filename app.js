cdf2271296ee4285f7074d612225ec535ac458e8
Johan Grandas <dampig14@gmail.com>
2023-04-06 17:45:33 -0500

    provider

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var app = angular.module("app", []);
 
-app.controller("emp", ["$scope", "calcFactory", function($scope, calcFactory) {
+app.controller("emp", ["$scope", "calcService", function($scope, calcService) {
   $scope.a = 10;
   $scope.b = 20;
 
   $scope.doSum = function () {
     // $scope.sum = parseInt($scope.a) + parseInt($scope.b);
-    $scope.sum = calcFactory.getSum($scope.a, $scope.b)
+    $scope.sum = calcService.getSum($scope.a, $scope.b)
 
   }
 
@@ -36,14 +36,63 @@ app.controller("emp", ["$scope", "calcFactory", function($scope, calcFactory) {
 }]);
 
 
-app.factory('calcFactory', ['$http', '$log', function($http, $log){
-  $log.log('instanting calcFactory..')
+// app.factory('calcFactory', ['$http', '$log', function($http, $log){
+//   $log.log('instanting calcFactory..')
 
-  var oCalcService = {}
-  oCalcService.getSum = function(a, b) {
-    return parseInt(a) + parseInt(b);
+//   var oCalcService = {}
+//   oCalcService.getSum = function(a, b) {
+//     return parseInt(a) + parseInt(b);
+//   }
+
+//   return oCalcService;
+
+// }]);
+
+app.provider('calcService', function(){
+
+
+  this.config = function(url) {
+    baseUrl = url;
   }
 
-  return oCalcService;
+ 
+  this.$get = ['$log', '$http', function($log, $http){   
 
+    $log.log('instanting calcService..')
+
+
+    $http({
+      url: baseUrl + '/character',
+      method: 'GET'
+    }).then(function (resp) {
+      //success function
+      console.log(resp)
+
+    },
+    function (resp) {
+      //failure function
+      console.log(resp)
+      $log.error("ERROR occurred")
+
+    });
+
+
+    var oCalcService = {}
+    oCalcService.getSum = function(a, b) {
+      return parseInt(a) + parseInt(b);
+    }
+  
+    return oCalcService;
+
+
+
+
+  }]
+
+});
+
+
+app.config(['calcServiceProvider', function(calcServiceProvider){
+  console.log("service provider on")
+  calcServiceProvider.config('https://finalspaceapi.com/api/v0');
 }])
