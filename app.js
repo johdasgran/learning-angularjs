95fe9800a75389e4b2f4e92b3176b0baf2e7542f
Johan Grandas <dampig14@gmail.com>
2023-04-06 18:54:45 -0500

    ng-repeat

--- app.js
+++ app.js
@@ -1,98 +1,89 @@
 var app = angular.module("app", []);
 
-app.controller("emp", ["$scope", "calcService", function($scope, calcService) {
+app.controller("emp", ["$scope", "$http", function($scope, $http) {
   $scope.a = 10;
   $scope.b = 20;
 
-  $scope.doSum = function () {
-    // $scope.sum = parseInt($scope.a) + parseInt($scope.b);
-    $scope.sum = calcService.getSum($scope.a, $scope.b)
+
+  // $scope.doData = function() {
+  //   $scope.characters = getData.getCharacters()
+  // }
+
+
+  $http({
+    url: 'https://finalspaceapi.com/api/v0/character',
+    method: 'GET'
+  }).then(function (resp) {
+    //success function
+    console.log(resp)
+    $scope.characters = resp.data
+
+  },
+  function (resp) {
+    //failure function
+    console.log(resp)
 
   }
-
-  // $http({
-  //   url: 'https://finalspaceapi.com/api/v0/character',
-  //   method: 'GET'
-  // }).then(function (resp) {
-  //   //success function
-  //   console.log(resp)
-
-  // },
-  // function (resp) {
-  //   //failure function
-  //   console.log(resp)
-
-  // }
-  // )
+  )
 
 
-  // $scope.doDoubleAndSum = function () {
-  //   $scope.a = $scope.a * 2;
-  //   $scope.b = $scope.b * 2;
-  //   $scope.sum = parseInt($scope.a) + parseInt($scope.b);
-  // }
+
 
 
 }]);
 
 
-// app.factory('calcFactory', ['$http', '$log', function($http, $log){
-//   $log.log('instanting calcFactory..')
 
-//   var oCalcService = {}
-//   oCalcService.getSum = function(a, b) {
-//     return parseInt(a) + parseInt(b);
+// app.provider('getData', function(){
+
+
+//   this.config = function(url) {
+//     baseUrl = url;
 //   }
 
-//   return oCalcService;
-
-// }]);
-
-app.provider('calcService', function(){
-
-
-  this.config = function(url) {
-    baseUrl = url;
-  }
-
  
-  this.$get = ['$log', '$http', function($log, $http){   
+//   this.$get = ['$log', '$http',  function($log, $http){   
 
-    $log.log('instanting calcService..')
-
-
-    $http({
-      url: baseUrl + '/character',
-      method: 'GET'
-    }).then(function (resp) {
-      //success function
-      console.log(resp)
-
-    },
-    function (resp) {
-      //failure function
-      console.log(resp)
-      $log.error("ERROR occurred")
-
-    });
+//     $log.log('instanting getData..')
 
 
-    var oCalcService = {}
-    oCalcService.getSum = function(a, b) {
-      return parseInt(a) + parseInt(b);
-    }
+//     var characters = {}
+//     characters.getCharacters = function() {
+        
+//       $http({
+//         url: baseUrl + '/character',
+//         method: 'GET'
+//       }).then(function (resp) {
+//         //success function
+//         // console.log(resp)
+//         characters = resp.data
+
+//         console.log(characters)
+
+//         return characters
+        
+//       },
+//       function (resp) {
+//         //failure function
+//         console.log(resp)
+//         $log.error("ERROR occurred")
+
+//       });
+
+//     }
   
-    return oCalcService;
+//     return characters;
 
 
 
 
-  }]
+//   }]
 
-});
+// });
 
 
-app.config(['calcServiceProvider', function(calcServiceProvider){
-  console.log("service provider on")
-  calcServiceProvider.config('https://finalspaceapi.com/api/v0');
-}])
+
+// app.config(['getDataProvider', function(getDataProvider){
+//   console.log("service provider on")
+//   getDataProvider.config('https://finalspaceapi.com/api/v0');
+// }])
